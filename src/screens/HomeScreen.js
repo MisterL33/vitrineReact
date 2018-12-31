10901a18a1793e8dab0b9dd7f61c897d83dc853c
unknown <laurent.julienne@ynov.com>
2018-12-31 15:37:52 +0100

    build

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,6 +6,21 @@ import pointeur from '../images/doigt.png';
 import laurent from '../images/laurent.jpg';
 class Home extends Component {
 
+    componentDidMount() {
+        this.callBackendAPI()
+            .then(res => this.setState({ data: res.express }))
+            .catch(err => console.log(err));
+    }
+
+    callBackendAPI = async () => {
+        const response = await fetch('/express_backend');
+        const body = await response.json();
+
+        if (response.status !== 200) {
+            throw Error(body.message)
+        }
+        return body;
+    };
 
     render() {
         return (
